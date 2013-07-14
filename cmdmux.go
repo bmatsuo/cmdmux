bdce633009cb5a53b1828ead29223e2174bc4ece
Bryan Matsuo <bryan dot matsuo at gmail dot com>
2013-07-13 22:30:20 -0700

    doc fixes

--- cmdmux.go
+++ cmdmux.go
@@ -11,26 +11,32 @@ package cmdmux
 
 var defaultMux = NewMux()
 
+// call Exec() on the default mux.
 func Exec(name string, args []string) {
 	defaultMux.Exec(name, args)
 }
 
+// register a function with the default mux.
 func RegisterFunc(name string, cmd CommandFunc) error {
 	return defaultMux.RegisterFunc(name, cmd)
 }
 
+// register a Command with the default mux.
 func Register(name string, cmd Command) error {
 	return defaultMux.Register(name, cmd)
 }
 
+// retrieve the names of commands registered with the default mux.
 func Commands() []string {
 	return defaultMux.Commands()
 }
 
+// a sub command. the Exec() method may or may not call os.Exit().
 type Command interface {
 	Exec(name string, args []string)
 }
 
+// an function that implements Command.
 type CommandFunc func(string, []string)
 
 func (fn CommandFunc) Exec(name string, args []string) {
