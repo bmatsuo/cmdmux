4a3b801613f01a7b87f49e00140e20bc14348c5c
Bryan Matsuo <bryan dot matsuo at gmail dot com>
2013-07-16 22:45:38 -0700

    additional example

--- example_test.go
+++ example_test.go
@@ -2,15 +2,18 @@ package cmdmux
 
 import (
 	"fmt"
+	"strings"
 )
 
+// This running example is a todo list manager.
 func Example() {
 	CreateTodo := func(name string, args []string) {
-		fmt.Println("created:", args)
+		fmt.Println("created:", args[0])
 	}
-
 	ListTodos := func(name string, args []string) {
-		fmt.Println("milk", "eggs", "motor boat")
+		fmt.Println("milk")
+		fmt.Println("eggs")
+		fmt.Println("motor boat")
 	}
 
 	RegisterFunc("new", CreateTodo)
@@ -18,5 +21,11 @@ func Example() {
 
 	Exec("todo", []string{"new", "quinoa"}) // usually os.Args[0], os.Args[1:]
 	// Output:
-	// created: [quinoa]
+	// created: quinoa
+}
+
+func ExampleCommands() {
+	commands := Commands()
+	fmt.Printf("available commands: %s", strings.Join(commands, ", "))
+	// Output: available commands: new, list
 }
