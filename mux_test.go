c2cf101682537678617e6bdb70106a48f14789bb
Bryan Matsuo <bryan dot matsuo at gmail dot com>
2013-07-16 22:22:04 -0700

    add remaining mux tests

--- mux_test.go
+++ mux_test.go
@@ -53,5 +53,78 @@ func TestMuxRegister(t *testing.T) {
 	if err == nil {
 		t.Errorf("registered nil command")
 	}
+}
 
+func TestMuxExec(t *testing.T) {
+	defer func() {
+		if e := recover(); e != nil {
+			t.Fatal("panic;", e)
+		}
+	}()
+
+	x := 0
+	mux := NewMux()
+	err := mux.RegisterFunc("blah", func(name string, args []string) { x++ })
+	if err != nil {
+		t.Fatalf("couldn't register test function;", err)
+	}
+	mux.Exec("bleh", []string{"blah"})
+	if x == 0 {
+		t.Fatal("command did not execute")
+	}
+}
+
+func TestMuxExecUnknown(t *testing.T) {
+	defer func() {
+		if e := recover(); e != nil {
+			t.Fatal("panic;", e)
+		}
+	}()
+	mux := NewMux()
+	caught := false
+	mux.CmdUnknown = func(string, []string) {
+		caught = true
+	}
+	mux.Exec("bleh", []string{"meh"})
+	if !caught {
+		t.Fatal("error not caught")
+	}
+}
+
+func TestMuxExecMissing(t *testing.T) {
+	defer func() {
+		if e := recover(); e != nil {
+			t.Fatal("panic;", e)
+		}
+	}()
+	mux := NewMux()
+	caught := false
+	mux.CmdMissing = func(string, []string) {
+		caught = true
+	}
+	mux.Exec("bleh", []string{})
+	if !caught {
+		t.Fatal("error not caught")
+	}
+}
+
+func TestMuxCommands(t *testing.T) {
+	mux := NewMux()
+	commands := []string{
+		"123", "abc", "456",
+		"def", "789", "ghi",
+	}
+	for _, name := range commands {
+		mux.Register(name, noop)
+	}
+	_commands := mux.Commands()
+	if len(commands) != len(_commands) {
+		t.Errorf("commands don't match %d != %d", len(commands), len(_commands))
+	} else {
+		for i := range commands {
+			if commands[i] != _commands[i] {
+				t.Errorf("command %d doesn't match %q != %q", i, commands[i], _commands[i])
+			}
+		}
+	}
 }
